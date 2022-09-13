a5bdc39465746023c6038f8e5386081cdb1b914e
Nwabisa Milisant Masiko <99478913+Miss-Masiko@users.noreply.github.com>
2022-09-13 13:55:14 +0200

    end-term project files

---
+++ js/array-literal.js
@@ -0,0 +1,10 @@
+//1. Create an array called colors and assign it values to "white", "black", "Green";
+
+
+
+//2. Show the first item from the array using their index number.
+
+
+//remember to use innerHTML
+
+
---
+++ js/numeric-variable.js
@@ -0,0 +1,12 @@
+// create three variables to store the information needed, namely:
+
+// 1.the price of each letter = 5
+// 2. the quantity = 15
+// 3. calculate the total by multipying the price by quantity
+
+//4. Get the element with an id "cost"
+
+
+//lastly use innerHTML to update the total cost
+
+
